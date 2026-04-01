a815493bb29b9800561ef6fcf53eb546bb84fb1c
Bruno Leão <bruno.melosh@gmail.com>
2026-04-01 17:50:05 -0400

    criação da página de rastreamento

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RastreamentoController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -8,3 +9,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', HomeController::class);
+
+Route::get('/rastreamento', RastreamentoController::class);
